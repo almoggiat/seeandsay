0817bc14a85f837f7fdc29c4a0a10753bc1da2d4
tom <tomrudyk@gmail.com>
2025-10-16 08:52:24 +0300

    Tom Branch

--- openAI_API_tests.py
+++ openAI_API_tests.py
@@ -8,10 +8,10 @@ import logging
 load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPENAI_KEY")
 
-client = OpenAI(api_key= OPENAI_API_KEY)
+openAI_client = OpenAI(api_key= OPENAI_API_KEY)
 audio_file= open("/home/tom/PycharmProjects/seeandsay/q1_whyHappy.mp3", "rb")
 
-transcription = client.audio.transcriptions.create(
+transcription = openAI_client.audio.transcriptions.create(
     model="whisper-1",
     file=audio_file
 )
